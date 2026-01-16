84b7dfce0187eae90d34448ee4121e0c965e170f
Mathias <mhgonzalezalbarracin@gmail.com>
2026-01-16 14:14:54 -0300

    Added base project

---
+++ PedidosYa/src/main/java/org/example/FintechChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechChallengeApplication.class, args);
+    }
+
+}
